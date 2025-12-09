91a1fc86181a4b7ac0f1c4c416042adf82d58b2c
JSurey <meza.201120606@gmail.com>
2025-12-09 08:24:01 -0600

    Add CurrentUserContext and fetch user data
    Introduced CurrentUserContext to provide user data throughout the app. Updated App.jsx to fetch user info from the API and supply it via context. Refactored Main.jsx to consume user data from context and fetch cards from the API, replacing hardcoded values.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,30 @@
 import Header from './Header/Header.jsx';
 import Main from './Main/Main.jsx';
 import Footer from './Footer/Footer.jsx';
+import { useEffect, useState } from 'react';
+import { api }from '../utils/api.js';
+import CurrentUserContext from '../contexts/CurrentUserContext.js';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState({});
+  useEffect(() => {
+    api.getUserInfo()
+      .then((data) => {
+        setCurrentUser(data);
+      })
+      .catch((err) => {
+        console.log("No se logró obtener la información del usuario:", err);
+      });
+  }, []);
   return (
     <>
-      <div className="page">
-        <Header />
-        <Main />
-        <Footer />
-      </div>
+      <CurrentUserContext.Provider value={currentUser}>
+        <div className="page">
+          <Header />
+          <Main />
+          <Footer />
+        </div>
+      </CurrentUserContext.Provider>
     </>
   )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import Popup from "./components/Popup/Popup.jsx";
 import EditAvatar from "./components/Popup/EditAvatar/EditAvatar.jsx";
 import EditProfile from "./components/Popup/EditProfile/EditProfile.jsx";
@@ -6,64 +6,65 @@ import NewCard from "./components/Popup/NewCard/NewCard.jsx";
 import Card from "./components/Card/Card.jsx";
 import ImagePopup from "./components/Popup/ImagePopup/ImagePopup.jsx";
 import RemoveCard from "./components/Popup/RemoveCard/RemoveCard.jsx";
+import { api } from "../../utils/api.js";
+import  CurrentUserContext  from "../../contexts/CurrentUserContext.js";
 
-const cards = [
-  {
-    createdAt: "2025-11-21T20:45:19.593Z",
-    isLiked: true,
-    link: "https://www.eluniversal.com.mx/resizer/v2/FONKJJXRC5CJFGOSGZZRLIOQEU.jpg?auth=ba689b12e44e6f08fb6a4dd227df1e83ad47f0c23a95a9c5759787af6e0904b3&smart=true&width=1100&height=666",
-    name: "Oaxaca",
-    owner: "bb48559270a68cd71830251c",
-    _id:    "6920cf5f9910450019ded240"
-  },
-  {
-    createdAt: "2025-11-02T20:59:38.028Z",
-    isLiked: true,
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
-    name: "Lago de Braies",
-    owner: "bb48559270a68cd71830251c",
-    _id: "6907c63ab08083001ae894f2",
-  },
-  {
-    createdAt: "2025-11-02T20:59:06.021Z",
-    isLiked: false,
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
-    name: "Parque Nacional del Valnoise",
-    owner: "bb48559270a68cd71830251c",
-    _id: "6907c61ab08083001ae894ee"
-  },
-  {
-    createdAt: "2025-11-02T20:58:30.743Z",
-    isLiked: true,
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
-    name: "Latemar",
-    owner: "bb48559270a68cd71830251c",
-    _id: "6907c5f6b08083001ae894ea"
-  },
-  {
-    createdAt: "2025-11-02T20:57:29.857Z",
-    isLiked: true,
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
-    name: "Valle de Yosemite",
-    owner: "bb48559270a68cd71830251c",
-    _id: "6907c5b9b08083001ae894dc"
-  },
-  {
-    createdAt: "2025-11-02T20:56:28.600Z",
-    isLiked: true,
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
-    name: "Lago Louise",
-    owner: "bb48559270a68cd71830251c",
-    _id: "6907c57cb08083001ae894b1"
-  }
-];
+// const cards = [
+//   {
+//     createdAt: "2025-11-21T20:45:19.593Z",
+//     isLiked: true,
+//     link: "https://www.eluniversal.com.mx/resizer/v2/FONKJJXRC5CJFGOSGZZRLIOQEU.jpg?auth=ba689b12e44e6f08fb6a4dd227df1e83ad47f0c23a95a9c5759787af6e0904b3&smart=true&width=1100&height=666",
+//     name: "Oaxaca",
+//     owner: "bb48559270a68cd71830251c",
+//     _id:    "6920cf5f9910450019ded240"
+//   },
+//   {
+//     createdAt: "2025-11-02T20:59:38.028Z",
+//     isLiked: true,
+//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+//     name: "Lago de Braies",
+//     owner: "bb48559270a68cd71830251c",
+//     _id: "6907c63ab08083001ae894f2",
+//   },
+//   {
+//     createdAt: "2025-11-02T20:59:06.021Z",
+//     isLiked: false,
+//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+//     name: "Parque Nacional del Valnoise",
+//     owner: "bb48559270a68cd71830251c",
+//     _id: "6907c61ab08083001ae894ee"
+//   },
+//   {
+//     createdAt: "2025-11-02T20:58:30.743Z",
+//     isLiked: true,
+//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+//     name: "Latemar",
+//     owner: "bb48559270a68cd71830251c",
+//     _id: "6907c5f6b08083001ae894ea"
+//   },
+//   {
+//     createdAt: "2025-11-02T20:57:29.857Z",
+//     isLiked: true,
+//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+//     name: "Valle de Yosemite",
+//     owner: "bb48559270a68cd71830251c",
+//     _id: "6907c5b9b08083001ae894dc"
+//   },
+//   {
+//     createdAt: "2025-11-02T20:56:28.600Z",
+//     isLiked: true,
+//     link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+//     name: "Lago Louise",
+//     owner: "bb48559270a68cd71830251c",
+//     _id: "6907c57cb08083001ae894b1"
+//   }
+// ];
 
 export function Main() {
     const [popup, setPopup] = useState(null);
     const [selectedCard, setSelectedCard] = useState(null);
-    const [userName, setUserName] = useState("Jacques Cousteau");
-    const [userDescription, setUserDescription] = useState("Explorador");
-    const [userAvatar, setUserAvatar] = useState("https://pictures.s3.yandex.net/frontend-developer/common/ava.jpg");
+    const [cards, setCards] = useState([]);
+    const currentUser = useContext(CurrentUserContext);
 
     const newEditAvatar = {
       title: "Cambiar foto de perfil",
@@ -79,6 +80,16 @@ export function Main() {
       title: "Nuevo lugar",
       children: <NewCard />,
     };
+
+    useEffect(() => {
+      api.getInitialCards()
+        .then((data) => {
+          setCards(data);
+        })
+        .catch((err) => {
+          console.log("No se logró obtener las tarjetas:", err);
+        });
+    }, []);
 
     function handleOpenPopup(popup) {
       setPopup(popup);
@@ -104,16 +115,16 @@ export function Main() {
           <div>
             <div className="profile__info">
               <div className="profile__avatar-container">
-                <img name="avatar" src={userAvatar} className="profile__avatar" alt="imagen-usuario"/>
+                <img name="avatar" src={currentUser.avatar} className="profile__avatar" alt="imagen-usuario"/>
                 <span name="editImage" className="profile__edit-icon" type="button" onClick={() => handleOpenPopup(newEditAvatar)}></span>
               </div>
               <div>
                 <div className="profile__edition">
-                  <h2 className="profile__name">{userName}</h2>
+                  <h2 className="profile__name">{currentUser.name}</h2>
                   <button className="profile__edit-button" type="button" onClick={() => handleOpenPopup(newEditProfile)}></button>
               </div>
               <div>
-                <p className="profile__position">{userDescription}</p>
+                <p className="profile__position">{currentUser.about}</p>
               </div>
             </div>
             </div>
